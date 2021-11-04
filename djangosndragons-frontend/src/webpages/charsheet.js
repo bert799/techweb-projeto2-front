7247b6227ed39bf9300d0f6d81655c0bf4735189
bernardo Cunha Capoferri <bernardocc@al.insper.edu.br>
2021-11-04 17:29:52 -0300

    Mostra descrição, mas não atualiza

--- djangosndragons-frontend/src/webpages/charsheet.js
+++ djangosndragons-frontend/src/webpages/charsheet.js
@@ -7,22 +7,26 @@ import axios from "axios";
 
 function Charsheet () {
     const [races, setRaces] = useState([]);
-    const raca = document.getElementById('raca');
-    
+    const [raca, setRaca] = useState([]);
 
+    
     useEffect(() => {
         axios
         .get("https://api.open5e.com/races")
         .then((res) => setRaces(res.data.results));
     }, []);
-
     console.log(races)
     let raceList = races.length > 0
     	&& races.map((item, i) => {
       return (
-        <option key={i} value={item.slug}>{item.name}</option>
+        <option key={i} value={i}>{item.name}</option>
       )
     }, this);
+
+    function killme(){
+      setRaca(document.getElementById('raca'));
+      console.log(raca);
+    }
 
     function handlePrint() {
         console.log(raca.value);
@@ -32,12 +36,12 @@ function Charsheet () {
     return (
         <div>
             <input id='nome' type='text' placeholder='Insira o nome do seu personagem'></input>
-            <select defaultValue = {false} id='raca' name='oi'>
+            <select id='raca' onChange={killme}>
               <option value={false} selected disabled hidden>Escolha sua raça</option>
               {raceList}
             </select>
             {raca != false &&
-              <p>baba</p>
+              <p>{races[raca.value].desc}</p>
             }
             <button onClick={handlePrint}>Submeter</button>
         </div>
